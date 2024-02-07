773c2df5ba108d54e1c25b2ea645b0bdd8aa2a9a
Ömer F. Sözcü <omer.sozcu@lexutb.se>
2024-02-07 12:13:45 +0100

    test 2 is added

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -6,3 +6,6 @@ Console.WriteLine("This code is for testing Master Branch");
 
 // This codes are added from test-1 branch
 Console.WriteLine("Test-1 branch is added");
+
+// This codes are added from test-2 branch
+Console.WriteLine("Test-2 branch is added");
