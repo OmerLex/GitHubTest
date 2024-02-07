9955f8c5a7af67f5f23a303c1d00c4751204bdbd
Ömer F. Sözcü <omer.sozcu@lexutb.se>
2024-02-07 11:48:04 +0100

    #1 added some codes

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// This code added from Master branch
+Console.WriteLine("This code is for testing Master Branch");
